17e4c39a91f0f08375f5f120a84c27ce7d060e0c
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2022-04-05 08:43:12 -0400

    remove liveview thing

--- lib/code_hunt_web/endpoint.ex
+++ lib/code_hunt_web/endpoint.ex
@@ -10,8 +10,6 @@ defmodule CodeHuntWeb.Endpoint do
     signing_salt: "aWT88lSp"
   ]
 
-  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
