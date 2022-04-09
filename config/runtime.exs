789fa969de494ca09705941cb8c18e2436d43c1c
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2022-04-09 19:15:51 -0400

    config stuff

--- config/runtime.exs
+++ config/runtime.exs
@@ -36,18 +36,18 @@ if config_env() == :prod do
       """
 
   host = System.get_env("PHX_HOST") || raise "Missing ENV var: PHX_HOST. Set it to: mydomain.tld"
-  port = System.get_env("PORT") || raise "Missing ENV var: PORT"
-  port = String.to_integer(port)
+  listen_port = String.to_integer(System.get_env("LISTEN_PORT") || raise "Missing ENV var: LISTEN_PORT")
+  url_port = String.to_integer(System.get_env("URL_PORT") || raise "Missing ENV var: URL_PORT")
 
   config :code_hunt, CodeHuntWeb.Endpoint,
-    url: [host: host, port: 443],
+    url: [host: host, port: url_port],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
+      port: listen_port
     ],
     secret_key_base: secret_key_base
 
