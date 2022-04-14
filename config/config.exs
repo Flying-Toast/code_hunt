d945af893c351e19a28a9e3eb511a80c5c1756a5
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2022-04-13 21:41:12 -0400

    separate script file

--- config/config.exs
+++ config/config.exs
@@ -24,7 +24,7 @@ config :esbuild,
   version: "0.14.0",
   default: [
     args:
-      ~w(css/app.css --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+      ~w(css/app.css js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
