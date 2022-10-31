e326342daf35d90064aa6087bb0fbc2bee04a22e
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2022-10-31 19:28:04 -0400

    add .iex.exs

---
+++ .iex.exs
@@ -0,0 +1,16 @@
+import Ecto.Query
+alias CodeHunt.{
+  Repo,
+  Missions,
+  Missions.Mission,
+  Missions.Trophy,
+  Contest,
+  Contest.Player,
+  Hunting,
+  Hunting.CodeDrop,
+  Hunting.CodeSheet,
+  Site,
+  Site.ModMessage,
+  Telemetry,
+  Telemetry.Event
+}
